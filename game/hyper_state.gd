23c2cf003e8ce3b29e776be39bfe7aac68f48311
Tony Ivanov <telamohn@pm.me>
2024-07-10 04:41:49 +0200

    Fixed callback

--- game/hyper_state.gd
+++ game/hyper_state.gd
@@ -1,15 +1,16 @@
 extends Node
 
+var _on_peer_socket_cb = JavaScriptBridge.create_callback(_on_peer_socket)
 func _init():
 	print('RUN1: Godot Loading')
 	var console = JavaScriptBridge.get_interface("console")
 	if console != null:
 		console.log('Godot State starting...')
 		var loader = JavaScriptBridge.get_interface("bootloader")
-		loader.joinTopic("Hello", _on_peer_socket)
+		loader.joinTopic("Hello", _on_peer_socket_cb)
 		print("RUN2: Joining SWARM")
 	else:
 		print("No js-runtime detected")
-		
+
 func _on_peer_socket (args):
 	print("Peer Joined", args)
